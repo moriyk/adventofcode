919459acb3a75719056feaf422c28ef031a9b649
moriyk <5351183+moriyk@users.noreply.github.com>
2023-10-29 01:01:23 +0900

    Add day04

---
+++ 2022/day04.py
@@ -0,0 +1,18 @@
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    fully_contained = 0
+    overlaps = 0
+    
+    for line in f:
+        a, b = line.split(',')
+        a1, a2 = map(int, a.split('-'))
+        b1, b2 = map(int, b.split('-'))
+
+        if (b1 <= a1 and a2 <= b2) or (a1 <= b1 and b2 <= a2):
+            fully_contained += 1
+        if not ((a2 < b1) or (b2 < a1)):
+            overlaps += 1
+    
+    print(f"fully contained: {fully_contained}")
+    print(f"overlaps: {overlaps}")

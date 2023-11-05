f4caedd77aec47f013573d1ad657631a1621e142
moriyk <5351183+moriyk@users.noreply.github.com>
2023-11-06 01:22:03 +0900

    Add day07

---
+++ 2022/day07-1.py
@@ -0,0 +1,27 @@
+import sys
+
+path = ['']
+dirsize = {}
+
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        p = line.strip().split(' ')
+        if p[0] == '$':
+            if p[1] == 'cd':
+                if p[2] == '/':
+                    path = ['']
+                elif p[2] == '..':
+                    path.pop()
+                else:
+                    path.append(p[2])
+        elif p[0] == 'dir':
+            pass
+        else:
+            for i in range(len(path)):
+                key = '/'.join(path[:i+1])
+                if key not in dirsize:
+                    dirsize[key] = 0
+                dirsize[key] += int(p[0])
+
+    total = sum([x for x in dirsize.values() if x <= 100000])
+    print(f'Total size: {total}')
